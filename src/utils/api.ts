1b9e6458501f7cd2fb6b3ccc623c7de5331e64a7
Tobias Molinero <tobiasmolinero98@gmail.com>
2025-10-06 14:41:34 -0300

    asdasdad

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,13 +2,14 @@
 export async function fetchAPI(endpoint: string, options: RequestInit = {}) {
     // const API_URL = import.meta.env.PUBLIC_API_URL || 'http://localhost:3000/';
     const res = await fetch(`https://international-tracking.vercel.app${endpoint.replace(/\/$/, '')}`, options);
-    const data = await res.datos.json();
+    console.log(await res.json());
+    const data = await res.json();
     console.log(data);
     if(data.error === "NO_ACC_TKN") {
         alert('No tienes permiso para realizar esta acción. Por favor, inicia sesión.');
         window.location.replace('/login');
         return;
-    }
+    } 
 
     return data;
 }
