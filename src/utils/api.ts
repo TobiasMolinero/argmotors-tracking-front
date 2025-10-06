65b33899152c4092c263758062a0549f7817278d
Tobias Molinero <tobiasmolinero98@gmail.com>
2025-10-06 15:47:48 -0300

    asdsad

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,9 @@
 // @ts-nocheck
 export async function fetchAPI(endpoint: string, options: RequestInit = {}) {
     // const API_URL = import.meta.env.PUBLIC_API_URL || 'http://localhost:3000/';
-    const res = await fetch(`https://argmotors-tracking-back.vercel.app${endpoint}}`, options);
+    const res = await fetch(`https://argmotors-tracking-back.vercel.app${endpoint}`, options);
     const data = await res.json();
-    console.log(data);
+    
     if(data.error === "NO_ACC_TKN") {
         alert('No tienes permiso para realizar esta acción. Por favor, inicia sesión.');
         window.location.replace('/login');
