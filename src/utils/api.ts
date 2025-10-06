c49fbc0b0739fc58d41cbf3463e54a3e7155356b
Tobias Molinero <tobiasmolinero98@gmail.com>
2025-10-06 15:12:21 -0300

    asda

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck
 export async function fetchAPI(endpoint: string, options: RequestInit = {}) {
     // const API_URL = import.meta.env.PUBLIC_API_URL || 'http://localhost:3000/';
-    const res = await fetch(`https://international-tracking.vercel.app/${endpoint}}`, options);
+    const res = await fetch(`https://international-tracking.vercel.app${endpoint}}`, options);
     console.log(await res.json());
     const data = await res.json();
     console.log(data);
