b61ff83766a6445b897f09bde817c036dd24a8d2
Tobias Molinero <tobiasmolinero98@gmail.com>
2025-10-06 14:32:02 -0300

    asda

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,8 @@
+// @ts-nocheck
 export async function fetchAPI(endpoint: string, options: RequestInit = {}) {
     // const API_URL = import.meta.env.PUBLIC_API_URL || 'http://localhost:3000/';
     const res = await fetch(`https://international-tracking.vercel.app${endpoint.replace(/\/$/, '')}`, options);
-    const data = await res.json();
+    const data = await res.datos.json();
     console.log(data);
     if(data.error === "NO_ACC_TKN") {
         alert('No tienes permiso para realizar esta acción. Por favor, inicia sesión.');
