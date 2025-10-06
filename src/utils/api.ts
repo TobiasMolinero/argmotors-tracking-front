1be5de26bf9315e65b529bc5328f5ceed5d4b382
Tobias Molinero <tobiasmolinero98@gmail.com>
2025-10-06 15:31:41 -0300

    asasd

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@
 export async function fetchAPI(endpoint: string, options: RequestInit = {}) {
     // const API_URL = import.meta.env.PUBLIC_API_URL || 'http://localhost:3000/';
     const res = await fetch(`https://international-tracking.vercel.app${endpoint}}`, options);
-    console.log(res);
+    console.log(res.json());
     const data = await res.json();
     console.log(data);
     if(data.error === "NO_ACC_TKN") {
