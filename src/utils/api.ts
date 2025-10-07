230c8401784665037931d10f38500c277ed84dd6
Tobias Molinero <tobiasmolinero98@gmail.com>
2025-10-07 11:28:29 -0300

    asdasd

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,6 +2,7 @@
 export async function fetchAPI(endpoint: string, options: RequestInit = {}) {
     // const API_URL = import.meta.env.PUBLIC_API_URL || 'http://localhost:3000/';
     const res = await fetch(`https://argmotors-tracking-back.vercel.app${endpoint}`, options);
+    // const res = await fetch(`http://localhost:3000${endpoint}`, options);
     const data = await res.json();
     
     if(data.error === "NO_ACC_TKN") {
